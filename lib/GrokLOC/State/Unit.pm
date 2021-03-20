6da28dd73ed051cf3ddb373390d027d64128e276
Brad Clawsie <brad@b7j0c.org>
2021-03-19 21:42:25 -0500

    set up root org and user for unit env

--- lib/GrokLOC/State/Unit.pm
+++ lib/GrokLOC/State/Unit.pm
@@ -1,6 +1,7 @@
 package GrokLOC::State::Unit;
 use strictures 2;
 use Carp qw(croak);
+use Crypt::Digest::SHA256 qw(sha256_b64);
 use Crypt::Misc qw(random_v4uuid);
 use English qw(-no_match_vars);
 use File::Spec;
@@ -9,6 +10,7 @@ use Mojo::SQLite;
 use Test::Mock::Redis;
 use experimental qw(signatures);
 use GrokLOC::Env qw(:all);
+use GrokLOC::Models qw(:all);
 use GrokLOC::Schemas;
 use GrokLOC::Security::Crypt qw(:all);
 use GrokLOC::State;
@@ -24,12 +26,48 @@ sub init() {
     my $master = Mojo::SQLite->new($db) || croak "new db: $ERRNO";
     $master->migrations->name('app')->from_string($GrokLOC::Schemas::APP);
     $master->migrations->migrate(0)->migrate || croak "migrate: $ERRNO";
+
+    # Set up the initial test root org and user. Tests need these.
+    my $root_org  = $ENV{ROOT_ORG}  // croak 'unit env root org';
+    my $root_user = $ENV{ROOT_USER} // croak 'unit env root user';
+
+    # Insert root org/user without calling the Model packages - those require
+    # an initialized State instance, which isn't ready yet.
+    $master->db->insert(
+        $ORGS_TABLENAME,
+        {
+            id     => $root_org,
+            name   => 'root',
+            owner  => $root_user,
+            status => $STATUS_ACTIVE,
+        }
+    );
+
+    my $kdf_iterations = 1;
+    my ( $api_secret, $display, $email, $password ) =
+      ( random_v4uuid, random_v4uuid, random_v4uuid, random_v4uuid );
+    $master->db->insert(
+        $USERS_TABLENAME,
+        {
+            id                => $root_user,
+            api_secret        => $api_secret,
+            api_secret_digest => sha256_b64($api_secret),
+            display           => $display,
+            display_digest    => sha256_b64($display),
+            email             => $email,
+            email_digest      => sha256_b64($email),
+            org               => $root_org,
+            password => kdf( $password, salt($root_user), $kdf_iterations ),
+            status   => $STATUS_ACTIVE,
+        }
+    );
+
     return GrokLOC::State->new(
         master         => $master,
         replicas       => [$master],
         cache          => Test::Mock::Redis->new( server => $UNIT ),
-        kdf_iterations => 1,
-        root_org       => random_v4uuid,
+        kdf_iterations => $kdf_iterations,
+        root_org       => $root_org,
         key            => key(random_v4uuid),
     );
 }
