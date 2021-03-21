89798e1d85eeb2de2588c989f8dfbea26cd23420
Brad Clawsie <brad@b7j0c.org>
2021-03-21 17:26:01 -0500

    unfinished with_session

--- service/app/lib/App/Controller/Api/V0/Auth.pm
+++ service/app/lib/App/Controller/Api/V0/Auth.pm
@@ -1,7 +1,12 @@
 package App::Controller::Api::V0::Auth;
 use strictures 2;
 use Mojo::Base 'Mojolicious::Controller';
+use Syntax::Keyword::Try qw(try catch :experimental);
 use experimental qw(signatures);
+use GrokLOC::App qw(:all);
+use GrokLOC::Models qw(:all);
+use GrokLOC::Models::Org;
+use GrokLOC::Models::User;
 
 # ABSTRACT: Auth middleware for populating the stash.
 
@@ -9,6 +14,55 @@ our $VERSION   = '0.01';
 our $AUTHORITY = 'cpan:bclawsie';
 
 sub with_session( $c ) {
+    $c->stash( $STASH_AUTH => $AUTH_NONE );
+
+    # The X-GrokLOC-ID header must be present in order to look up the user.
+    unless ( $c->req->headers->header($X_GROKLOC_ID) ) {
+        $c->render( app_msg( 400, { error => 'missing:' . $X_GROKLOC_ID } ) );
+        return;
+    }
+    my $user_id = $c->req->headers->header($X_GROKLOC_ID);
+    my $user;
+    try {
+        $user =
+          GrokLOC::Models::User->read( $c->st->random_replica(), $user_id );
+    }
+    catch ($e) {
+        $c->app->log->error( 'internal error reading user ' . $user_id );
+        $c->render( app_msg( 500, { error => 'internal error' } ) );
+        return;
+    }
+    if ( !defined($user) || $user->_meta->status != $STATUS_ACTIVE ) {
+        $c->render( app_msg( 404, { error => 'user not found' } ) );
+        return;
+    }
+    my $org;
+    try {
+        $org =
+          GrokLOC::Models::Org->read( $c->st->random_replica(), $user->org );
+    }
+    catch ($e) {
+        $c->app->log->error( 'internal error reading org ' . $user->org );
+        $c->render( app_msg( 500, { error => 'internal error' } ) );
+        return;
+    }
+    if ( !defined($org) || $org->_meta->status != $STATUS_ACTIVE ) {
+        $c->render( app_msg( 400, { error => 'org not found' } ) );
+        return;
+    }
+    my $auth_level = $AUTH_USER;
+
+    if ( $org->id eq $c->st->root_org ) {
+        $auth_level = $AUTH_ROOT;
+    }
+    elsif ( $org->owner eq $user->id ) {
+        $auth_level = $AUTH_ORG;
+    }
+
+    if ( !$c->req->headers->header($X_GROKLOC_TOKEN) ) {
+
+    }
+    $c->stash( $STASH_AUTH => $auth_level );
     return 1;
 }
 
