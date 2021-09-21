6aa2df5453ac771c648e6dc74a52de26d2a7c5f4
Brad Clawsie <brad@b7j0c.org>
2021-09-21 01:29:00 +0000

    add

---
+++ service/app/lib/App/Controller/Api/V0/Repository.pm
@@ -0,0 +1,27 @@
+package App::Controller::Api::V0::Repository;
+use strictures 2;
+use Mojo::Base 'Mojolicious::Controller';
+use GrokLOC::Models::Repository;
+
+# ABSTRACT: Repository operations.
+
+our $VERSION   = '0.01';
+our $AUTHORITY = 'cpan:bclawsie';
+
+1;
+
+__END__
+
+=head1 NAME
+
+App::Controller::Api::V0::Repository
+
+=head1 SYNOPSIS
+
+Repository operations.
+
+=head1 DESCRIPTION
+
+Repository operations.
+
+=cut
